3739c252a2c9286192d7d326ce5c56e4e03d744a
Ankush Verma <55029938+ankush99verma@users.noreply.github.com>
2022-01-31 21:46:22 -0800

    Update server2.js

--- server2.js
+++ server2.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
-const hostname = '192.168.211.128';
+const hostname = '192.168.177.130';
 const port = 5001;
 
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
   	res.setHeader('Content-Type', 'text/plain');
-  	res.end('2nd Nodejs App is Up and Running!\n');
+  	res.end('2nd Nodejs server is Up and Running!\n');
 });
 
 server.listen(port, hostname, () => {
