c95c88d88d794db6acf3ade9d020ab68b3b9753d
ankush <v99ankush@gmail.com>
2022-02-11 14:21:15 +0530

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express= require('express');
 var app = express();
 
 app.get('/', function (req,res) {
-  res.send('Hey!, this is a demo commit for testing ');
+  res.send('Hey!, this is a demo commit for testing');
 });
 
 var server = app.listen(3000, function () {
